8e0b774a853fcc4a89d7ffce42144430ef17066e
stealthkong <stealth_kong@yahoo.com>
2016-04-03 17:19:14 -0500

    2D-player-controller:Fixed
    Still untested, but a working version of the first player controller.
    Needs testing with a player character.

--- playerController.cs
+++ playerController.cs
@@ -1,23 +1,28 @@
 using UnityEngine;
 using System.Collections;
 
-public class playerController : MonoBehavior
+public class playerController : MonoBehaviour
 {
-	public float maxSpeed = 10f;
-	bool facingRight = true;
+	public float maxSpeed = 10f; // Variable for the fastes spped the player can travel
+	bool facingRight = true;	// Determines which way the player is facing
 	
-	Animator anim;
+	public Animator anim;	// References the players animator component
+
+	float groundRadius = 0.2f;	// Radius to determine if the player is grounded
+	public LayerMask whatIsGround;	//Mask that determines what is ground to the character
+	public float jumpForce = 700f;	// The force added when jumping
+	public float crouchSpeed = .36f;	// maxSpeed applied to crouchiing
 	
-	bool grounded = false;
-	public Transfomr groundCheck;
-	float groundRadius = 0.2f;
-	public LayerMask whatIsGround;
-	public float jumpForce = 700f;
+	public bool grounded = false;	// Variable for whether the player is grounded or not
+	public Transform groundCheck;	// Determines whether the player is grounded
+	public Transform ceilingCheck;	// Determines the spot where the 'ceiling' of the level is
 	
 	bool doubleJump = false; // Double jump component
 	
-	void start()
+	public void Awake()
 	{
+		groundCheck = transform.Find ("GroundCheck");
+		ceilingCheck = transform.Find ("CeilingCheck");
 		anim = GetComponent<Animator>();
 	}
 	
@@ -30,32 +35,33 @@ public class playerController : MonoBehavior
 			if(grounded)
 				doubleJump = false;
 			
-			anim.SetFloat("vSpeed", rigibody2D.velocity.y);
+			anim.SetFloat("vSpeed", GetComponent<Rigidbody2D>().velocity.y);
 		
 			// component to prevent player from controlling character midair. I'm leaving this in here
 			// incase this is something we would want to implement, but for a game like what we
 			// are making, this might help the player with precision controls
+
 			//if(!grounded) return;
 			
 			float move = Input.GetAxis("Horizontal");
 			
 			anim.SetFloat("Speed", Mathf.Abs(move));
 			
-			rigibody2D.Velocity = new Vector2(move * maxSpeed, rigibody2D.velocity.y);
+		GetComponent<Rigidbody2D>().velocity = new Vector2(move * maxSpeed, GetComponent<Rigidbody2D>().velocity.y);
 			
-			if(move > 0 &&! facingRight)
+			if(move > 0 &&!facingRight)
 				Flip();
 			else if(move < 0 && facingRight)
 				Flip();
 	}
-	
+
 	void Update()
 	{
-		if((grounded || !doubleJump) && Input.GetKeyDown)
+		if((grounded || !doubleJump) && Input.GetKeyDown(KeyCode.Space))
 		{
 			anim.SetBool("Ground", false);
-			rigibody2D.AddForce(new, Vector2(0, jumpForce));
-			
+			GetComponent<Rigidbody2D>().AddForce(new Vector2(0, jumpForce));
+
 			if(!doubleJump && !grounded)
 				doubleJump = true;
 		}
