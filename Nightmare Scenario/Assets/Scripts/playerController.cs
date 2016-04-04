8b6d5ac475934a81b2009fd7ef47c0e18f59656b
stealthkong <stealth_kong@yahoo.com>
2016-04-04 17:05:14 -0500

    characterController name change and test
    - changed the name of playerController to characterController
    
    - characterController tested; the simple movement mechanics are working
    well, but the double jump feature needs work. The character can
    continuously jump currently.

--- Nightmare Scenario/Assets/Scripts/playerController.cs
+++ Nightmare Scenario/Assets/Scripts/playerController.cs
@@ -1,78 +1,36 @@
+using System;
 using UnityEngine;
-using System.Collections;
 
-public class playerController : MonoBehaviour
+namespace public class playerController : MonoBehaviour
 {
-	public float maxSpeed = 10f; // Variable for the fastes spped the player can travel
-	bool facingRight = true;	// Determines which way the player is facing
-	
-	public Animator anim;	// References the players animator component
+    [RequireComponent(typeof (PlatformerCharacter2D))]
+    public class Platformer2DUserControl : MonoBehaviour
+    {
+        private PlatformerCharacter2D m_Character;
+        private bool m_Jump;
 
-	float groundRadius = 0.2f;	// Radius to determine if the player is grounded
-	public LayerMask whatIsGround;	//Mask that determines what is ground to the character
-	public float jumpForce = 700f;	// The force added when jumping
-	public float crouchSpeed = .36f;	// maxSpeed applied to crouchiing
-	
-	public bool grounded = false;	// Variable for whether the player is grounded or not
-	public Transform groundCheck;	// Determines whether the player is grounded
-	public Transform ceilingCheck;	// Determines the spot where the 'ceiling' of the level is
-	
-	bool doubleJump = false; // Double jump component
-	
-	public void Awake()
-	{
-		groundCheck = transform.Find ("GroundCheck");
-		ceilingCheck = transform.Find ("CeilingCheck");
-		anim = GetComponent<Animator>();
-	}
-	
-	void FixedUpdate()
-	{
-			grounded = Physics2D.OverlapCircle(groundCheck.position, groundRadius, whatIsGround);
-			anim.SetBool("Ground", grounded);
-			
-			// Sets doubleJump variable for later use
-			if(grounded)
-				doubleJump = false;
-			
-			anim.SetFloat("vSpeed", GetComponent<Rigidbody2D>().velocity.y);
-		
-			// component to prevent player from controlling character midair. I'm leaving this in here
-			// incase this is something we would want to implement, but for a game like what we
-			// are making, this might help the player with precision controls
 
-			//if(!grounded) return;
-			
-			float move = Input.GetAxis("Horizontal");
-			
-			anim.SetFloat("Speed", Mathf.Abs(move));
-			
-		GetComponent<Rigidbody2D>().velocity = new Vector2(move * maxSpeed, GetComponent<Rigidbody2D>().velocity.y);
-			
-			if(move > 0 &&!facingRight)
-				Flip();
-			else if(move < 0 && facingRight)
-				Flip();
-	}
+        private void Awake()
+        {
+            m_Character = GetComponent<PlatformerCharacter2D>();
+        }
 
-	void Update()
-	{
-		if((grounded || !doubleJump) && Input.GetKeyDown(KeyCode.Space))
-		{
-			anim.SetBool("Ground", false);
-			GetComponent<Rigidbody2D>().AddForce(new Vector2(0, jumpForce));
 
-			if(!doubleJump && !grounded)
-				doubleJump = true;
-		}
-	}
-	
-	void Flip()
-	{
-		facingRight = !facingRight;
-		Vector3 theScale = transform.localScale;
-		theScale.x *= -1;
-		transform.localScale = theScale;
-	}
-	
+        private void Update()
+        {
+            if (!m_Jump)
+            {
+                m_Jump = CrossPlatformInputManager.GetButtonDown("Jump");
+            }
+        }
+
+
+        private void FixedUpdate()
+        {
+            bool crouch = Input.GetKey(KeyCode.LeftControl);
+            float h = CrossPlatformInputManager.GetAxis("Horizontal");
+            m_Character.Move(h, crouch, m_Jump);
+            m_Jump = false;
+        }
+    }
 }
