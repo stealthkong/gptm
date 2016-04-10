5a5772631febd27eaeea3cba40107a79f2c3c1b7
stealthkong <stealth_kong@yahoo.com>
2016-04-10 17:03:33 -0500

    Template scenes and controller fix
    Created a template scene so we have our scenes in the same size and
    simplified the main character controller.

--- Nightmare Scenario/Assets/Scripts/playerController.cs
+++ Nightmare Scenario/Assets/Scripts/playerController.cs
@@ -1,36 +1,50 @@
-using System;
-using UnityEngine;
+﻿using UnityEngine;
+using System.Collections;
 
-namespace public class playerController : MonoBehaviour
+public class playerController : MonoBehaviour 
 {
-    [RequireComponent(typeof (PlatformerCharacter2D))]
-    public class Platformer2DUserControl : MonoBehaviour
-    {
-        private PlatformerCharacter2D m_Character;
-        private bool m_Jump;
 
+	//Movement
+	public float speed;
+	public float jump;
+	float moveVelocity;
 
-        private void Awake()
-        {
-            m_Character = GetComponent<PlatformerCharacter2D>();
-        }
+	//Grounded Vars
+	bool grounded = true;
+	
+	void Update () 
+	{
+		//Jumping
+		if (Input.GetKeyDown (KeyCode.Space) || Input.GetKeyDown (KeyCode.UpArrow) || Input.GetKeyDown (KeyCode.Z) || Input.GetKeyDown (KeyCode.W)) 
+		{
+			if(grounded)
+			{
+				GetComponent<Rigidbody2D> ().velocity = new Vector2 (GetComponent<Rigidbody2D> ().velocity.x, jump);
+			}
+		}
 
+		moveVelocity = 0;
 
-        private void Update()
-        {
-            if (!m_Jump)
-            {
-                m_Jump = CrossPlatformInputManager.GetButtonDown("Jump");
-            }
-        }
+		//Left Right Movement
+		if (Input.GetKey (KeyCode.LeftArrow) || Input.GetKey (KeyCode.A)) 
+		{
+			moveVelocity = -speed;
+		}
+		if (Input.GetKey (KeyCode.RightArrow) || Input.GetKey (KeyCode.D)) 
+		{
+			moveVelocity = speed;
+		}
 
+		GetComponent<Rigidbody2D> ().velocity = new Vector2 (moveVelocity, GetComponent<Rigidbody2D> ().velocity.y);
 
-        private void FixedUpdate()
-        {
-            bool crouch = Input.GetKey(KeyCode.LeftControl);
-            float h = CrossPlatformInputManager.GetAxis("Horizontal");
-            m_Character.Move(h, crouch, m_Jump);
-            m_Jump = false;
-        }
-    }
+	}
+	//Check if Grounded
+	void OnTriggerEnter2D()
+	{
+		grounded = true;
+	}
+	void OnTriggerExit2D()
+	{
+		grounded = false;
+	}
 }
