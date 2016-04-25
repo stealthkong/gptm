6833c40567ba8fbecced6006eb10c3ed5ced34a5
stealthkong <stealth_kong@yahoo.com>
2016-04-24 21:58:45 -0500

    Level 6, Spike added
    In this iteration:
    
    - Level 6 functionality has been implemented, bare bones style
    
    - Spikes have been created as a prefab for use in levels.

---
+++ Nightmare Scenario/Assets/Scripts/laserBullet.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class laserBullet : MonoBehaviour {
+
+	public float speed;
+	public Vector2 direction;
+	public bool isReady;
+	public Vector2 position;
+	public Vector2 min;
+	public Vector2 max;
+
+	void Awake()
+	{
+		speed = 5f;
+		isReady = false;
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	void SetDirection(Vector2 direction)
+	{
+		direction = direction.normalized;
+
+		isReady = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (isReady) {
+			position = transform.position;
+
+			position += direction * speed * Time.deltaTime;
+
+			transform.position = position;
+
+			min = Camera.main.ViewportToWorldPoint(new Vector2(0,0));
+
+			max = Camera.main.ViewportToWorldPoint(new Vector2(1,1));
+
+			if((transform.position.x < min.x) || (transform.position.x > max.x) || (transform.position.y < min.y) || (transform.position.y > max.y))
+			{
+				Destroy (gameObject);
+			}
+
+		}
+	}
+}
---
+++ Nightmare Scenario/Assets/Scripts/laserGun.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class laserGun : MonoBehaviour {
+
+	public GameObject laserBullet;
+	public GameObject player;
+
+
+	// Use this for initialization
+	void Start () {
+		Invoke ("fireLaser", 1f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void fireLaser()
+	{
+		player = GameObject.Find ("player");
+
+		if (player != null) {
+			GameObject blast =(GameObject)Instantiate (laserBullet);
+
+			blast.transform.position = transform.position;
+
+			Vector2 direction = player.transform.position - blast.transform.position;
+
+			//blast.GetComponent<laserBullet>().SetDirection(direction);
+		}
+	}
+}
