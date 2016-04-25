dc92020b4ec4b1f93fb6bd9ffaef8df0f1db5e54
stealthkong <stealth_kong@yahoo.com>
2016-04-24 19:33:21 -0500

    New first level added
    This iteration includes:
    
    - This has a new first level implemented as a tutorial level. This way
    the player isn't thrust into death, and can learn to practice the
    controls.

---
+++ Nightmare Scenario/Assets/Scripts/messageBoardScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class messageBoardScript : MonoBehaviour {
+
+	public GUIText heading;
+	public GUIText message1;
+	public GUIText message2;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void OnTriggerCollider2D(Collider2D other)
+	{
+		if (other.tag == "Player") {
+			heading.text = "How to not die:";
+			message1.text = "Controls:Jump: Space Button Movement: a and d Pause: p button";
+			message2.text= "Objectives:- Head to the red door- don't touch anything- seriously, don't touch anything";
+		}
+	}
+}
